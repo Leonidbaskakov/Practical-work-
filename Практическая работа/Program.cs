aac7be355271426b022f39191786f190e33aac22
leonid <bskakaw2014@yandex.ru>
2022-09-14 01:02:29 +0300

    Добавлена проверка повторяемости элементов

--- Практическая работа/Program.cs
+++ Практическая работа/Program.cs
@@ -5,8 +5,8 @@
 Console.WriteLine("Введите количество массивов");
 int countmas = Convert.ToInt32(Console.ReadLine());
 
-int nummas = 1;
-while (nummas != countmas)
+int nummas = 1; // счётчик массива, каждый раз в конце увеличивается на 1
+while (nummas != countmas + 1)
 {
     int n = new Random().Next(5, 7); //Размер массива
 
@@ -30,25 +30,36 @@ while (nummas != countmas)
         }
         Console.Write($"{array[array.Length - 1]}]");
     }
-
     string[] arrays = FillArray(n); // заполнение нашего массива
-
     PrintArr(arrays);
+
     Console.WriteLine();
-    Console.WriteLine("Отсортированный массив");
+    Console.WriteLine("Отфильтрованный массив");
 
     string[] FillNewArray(string[] array, int min, int max) // на вход принимаем наш массив
     {
         int f = new Random().Next(min, max); //создаем размер нового массива (в нашем случае от 1 до 3)
         string[] newarray = new string[f];
+
         for (int i = 0; i < newarray.Length; i++)
         {
             newarray[i] = array[new Random().Next(0, array.Length - 1)]; //заполнение массива идет случайнымми элементами
         }
+
+        int n = 0;
+        newarray[n] = array[new Random().Next(0, array.Length - 1)];
+        for (int i = 1; i < newarray.Length; i++)
+        {
+            if (newarray[n] == newarray[i])
+            {
+                newarray[i] = array[new Random().Next(0, array.Length - 1)];
+                n = i;
+            }
+        }
         return newarray;
     }
 
-    string[] newarr = FillNewArray(arrays, 1, 3);
+    string[] newarr = FillNewArray(arrays, 3, 3);
     PrintArr(newarr);
     nummas++;
     Console.WriteLine();
